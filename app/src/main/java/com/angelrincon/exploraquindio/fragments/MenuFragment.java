66230d7c97fd294484f46853169689125875663c
acalderon474 <acalderon@poligran.edu.co>
2026-09-22 15:53:52 -0500

    Proyecto base Explora Quindio - inicio segunda entrega

---
+++ app/src/main/java/com/angelrincon/exploraquindio/fragments/MenuFragment.java
@@ -0,0 +1,12 @@
+package com.angelrincon.exploraquindio.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.angelrincon.exploraquindio.R;
+
+public class MenuFragment extends Fragment {
+
+    public MenuFragment() {
+        super(R.layout.fragment_menu);
+    }
+}
---
+++ app/src/main/java/com/angelrincon/exploraquindio/fragments/ProfileFragment.java
@@ -0,0 +1,12 @@
+package com.angelrincon.exploraquindio.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.angelrincon.exploraquindio.R;
+
+public class ProfileFragment extends Fragment {
+
+    public ProfileFragment() {
+        super(R.layout.fragment_profile);
+    }
+}
