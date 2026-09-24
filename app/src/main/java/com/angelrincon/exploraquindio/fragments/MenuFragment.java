c6fdd026fb5eaef69b759e2e1b42941b62973d88
acalderon474 <acalderon@poligran.edu.co>
2026-09-24 02:01:40 -0500

    Se vincula controles y navegación de Perfil y Fotos

--- app/src/main/java/com/angelrincon/exploraquindio/fragments/MenuFragment.java
+++ app/src/main/java/com/angelrincon/exploraquindio/fragments/MenuFragment.java
@@ -1,12 +1,53 @@
 package com.angelrincon.exploraquindio.fragments;
 
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
 import com.angelrincon.exploraquindio.R;
 
 public class MenuFragment extends Fragment {
 
+    private Button btnPerfil;
+    private Button btnFotos;
+    private Button btnVideo;
+    private Button btnWeb;
+    private Button btnBotones;
+
     public MenuFragment() {
         super(R.layout.fragment_menu);
+    }
+
+    @Override
+    public void onViewCreated(
+            @NonNull View view,
+            @Nullable Bundle savedInstanceState
+    ) {
+        super.onViewCreated(view, savedInstanceState);
+
+        btnPerfil = view.findViewById(R.id.btnPerfil);
+        btnFotos = view.findViewById(R.id.btnFotos);
+        btnVideo = view.findViewById(R.id.btnVideo);
+        btnWeb = view.findViewById(R.id.btnWeb);
+        btnBotones = view.findViewById(R.id.btnBotones);
+
+        btnPerfil.setOnClickListener(v ->
+                cambiarFragment(new ProfileFragment())
+        );
+
+        btnFotos.setOnClickListener(v ->
+                cambiarFragment(new PhotosFragment())
+        );
+    }
+
+    private void cambiarFragment(Fragment fragment) {
+        getParentFragmentManager()
+                .beginTransaction()
+                .replace(R.id.contentContainer, fragment)
+                .commit();
     }
 }
