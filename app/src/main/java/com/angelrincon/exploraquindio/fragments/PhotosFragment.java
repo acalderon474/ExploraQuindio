c255d6d831e319c8d97620b7ba03a4ad0a5bc837
acalderon474 <acalderon@poligran.edu.co>
2026-09-23 19:53:06 -0500

    Se crea estructura inicial de PhotosFragment

---
+++ app/src/main/java/com/angelrincon/exploraquindio/fragments/PhotosFragment.java
@@ -0,0 +1,12 @@
+package com.angelrincon.exploraquindio.fragments;
+
+import androidx.fragment.app.Fragment;
+
+import com.angelrincon.exploraquindio.R;
+
+public class PhotosFragment extends Fragment {
+
+    public PhotosFragment() {
+        super(R.layout.fragment_photos);
+    }
+}
